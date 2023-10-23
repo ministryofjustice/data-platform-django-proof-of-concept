07af9ec31e419a6ae326d775427bf1aabe56b7e9
julialawrence <julia.lawrence@digital.justice.gov.uk>
2023-10-23 08:49:03 +0100

    Debugging proxy route and formatting.

--- app/cluster_manager.py
+++ app/cluster_manager.py
@@ -11,8 +11,9 @@ except ConfigException:
     try:
         config.load_kube_config()
     except ConfigException:
-
-        print("Could not configure Kubernetes client. This script must be run within a cluster or with access to a valid kubeconfig file.")
+        print(
+            "Could not configure Kubernetes client. This script must be run within a cluster or with access to a valid kubeconfig file."
+        )
         exit(1)
 
 # Create instances of the API classes
@@ -20,15 +21,17 @@ api_instance = client.CoreV1Api()
 api_instance_rbac = client.RbacAuthorizationV1Api()
 api_instance_apps = client.AppsV1Api()
 
+
 def sanitize_username(username):
     # Remove or replace invalid characters here, and return a 'safe' version of the username
-    sanitized = ''.join(e for e in username if e.isalnum())
+    sanitized = "".join(e for e in username if e.isalnum())
     return sanitized
 
+
 def create_service_account(user_id):
-    namespace = 'dataaccessmanager'
+    namespace = "dataaccessmanager"
     sanitized_user_id = sanitize_username(user_id)
-    name = f'vscode-sa-{sanitized_user_id}'
+    name = f"vscode-sa-{sanitized_user_id}"
 
     # Define the service account with the IRSA role annotation
     body = client.V1ServiceAccount(
@@ -36,56 +39,53 @@ def create_service_account(user_id):
             name=name,
             annotations={
                 # Replace with your actual ARN for the IAM role
-                'eks.amazonaws.com/role-arn': 'arn:aws:iam::123456789012:role/role-name'
-            }
+                "eks.amazonaws.com/role-arn": "arn:aws:iam::123456789012:role/role-name"
+            },
         )
     )
 
     # Create the service account in the specified namespace
     api_instance.create_namespaced_service_account(namespace, body)
 
+
 def deploy_vscode_server(user_id):
-    namespace = 'dataaccessmanager'
+    namespace = "dataaccessmanager"
     sanitized_user_id = sanitize_username(user_id)
-    name = f'vscode-server-{sanitized_user_id}'
-    service_account_name = f'vscode-sa-{sanitized_user_id}'
+    name = f"vscode-server-{sanitized_user_id}"
+    service_account_name = f"vscode-sa-{sanitized_user_id}"
 
     # Define the pod spec with the associated service account
     pod_spec = client.V1PodSpec(
         service_account_name=service_account_name,
         containers=[
             client.V1Container(
-                name='vscode-server',
-                image='codercom/code-server:latest',
-                ports=[client.V1ContainerPort(container_port=8080)]
+                name="vscode-server",
+                image="codercom/code-server:latest",
+                ports=[client.V1ContainerPort(container_port=8080)],
             )
-        ]
+        ],
     )
 
     # Define the pod's metadata
     metadata = client.V1ObjectMeta(name=name)
 
     # Create the pod specification
-    pod = client.V1Pod(
-        api_version="v1",
-        kind="Pod",
-        metadata=metadata,
-        spec=pod_spec
-    )
+    pod = client.V1Pod(api_version="v1", kind="Pod", metadata=metadata, spec=pod_spec)
 
     # Deploy the pod in Kubernetes
     api_instance.create_namespaced_pod(namespace, pod)
 
+
 def create_service_for_vscode(user_id):
-    namespace = 'dataaccessmanager'
+    namespace = "dataaccessmanager"
     sanitized_user_id = sanitize_username(user_id)
-    name = f'vscode-service-{sanitized_user_id}'
+    name = f"vscode-service-{sanitized_user_id}"
 
     # Define the service's spec
     spec = client.V1ServiceSpec(
-        selector={'app': f'vscode-server-{sanitized_user_id}'},
-        ports=[client.V1ServicePort(protocol='TCP', port=80, target_port=8080)],
-        type='ClusterIP'
+        selector={"app": f"vscode-server-{sanitized_user_id}"},
+        ports=[client.V1ServicePort(protocol="TCP", port=80, target_port=8080)],
+        type="ClusterIP",
     )
 
     # Create the service specification
@@ -93,26 +93,28 @@ def create_service_for_vscode(user_id):
         api_version="v1",
         kind="Service",
         metadata=client.V1ObjectMeta(name=name),
-        spec=spec
+        spec=spec,
     )
 
     # Create the service in Kubernetes
     api_instance.create_namespaced_service(namespace, service)
+
 
 def wait_for_pod_ready(namespace, pod_name):
     # Create a watch object for Pod events
     w = watch.Watch()
 
     for event in w.stream(api_instance.list_namespaced_pod, namespace):
-        pod = event['object']
-        if pod.metadata.name == pod_name and pod.status.phase == 'Running':
+        pod = event["object"]
+        if pod.metadata.name == pod_name and pod.status.phase == "Running":
             w.stop()
             return True  # Pod is now running
-        elif pod.metadata.name == pod_name and pod.status.phase == 'Failed':
+        elif pod.metadata.name == pod_name and pod.status.phase == "Failed":
             w.stop()
             return False  # Pod failed to start
 
     return False  # Default case, though your logic might differ based on how you want to handle timeouts
+
 
 def launch_vscode_for_user(user_id):
     # Step 1: Create a service account for the user
@@ -126,8 +128,8 @@ def launch_vscode_for_user(user_id):
 
     # Step 4: Wait for the pod to be in the 'Running' state
     sanitized_user_id = sanitize_username(user_id)
-    pod_name = f'vscode-server-{sanitized_user_id}'
-    namespace = 'dataaccessmanager'
+    pod_name = f"vscode-server-{sanitized_user_id}"
+    namespace = "dataaccessmanager"
 
     if wait_for_pod_ready(namespace, pod_name):
         print("VS Code server is ready for use.")
